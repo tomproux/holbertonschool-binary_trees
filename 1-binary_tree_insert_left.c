b451ba27848ea941d59892b485ce316816899ebb
ValPQT <valentinpasquiet@gmail.com>
2025-12-09 09:13:18 +0000

    add new files

---
+++ 1-binary_tree_insert_left.c
@@ -0,0 +1,30 @@
+#include <stdlib.h>
+#include "binary_trees.h"
+
+binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
+{
+	binary_tree_t *new_node;
+
+	/* verifie si parent == NULL*/
+	if (parent == NULL)
+		return (NULL);
+	/*alloue un espace memoire*/
+	new_node = (binary_tree_t *) malloc(sizeof(binary_tree_t));
+	if (!new_node)
+	{
+		new_node->n = value;
+		new_node->parent = parent;
+		new_node->left = NULL;
+		new_node->right = NULL;
+	}
+	/* verifie le cas ou le parent a deja un enfant gauche*/
+	if (parent->left != NULL)
+
+       {
+	       new_node->left = parent->left;
+	       new_node->left->parent = new_node;
+       }
+
+	parent->left = new_node;
+	return (new_node);
+}
