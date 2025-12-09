b6a9e799794b7b74e1610a1024999bce522ec303
ValPQT <valentinpasquiet@gmail.com>
2025-12-09 09:58:56 +0000

    feat : < add the new file 2-binary_tree_insert_right.c >

---
+++ 2-binary_tree_insert_right.c
@@ -0,0 +1,31 @@
+#include <stdlib.h>
+#include "binary_trees.h"
+
+binary_tree_t *binary_tree_insert_right(binary_tree_t *parent, int value)
+{
+	binary_tree_t *new_node = NULL;
+
+	/* verifie si parent == NULL*/
+	if (parent == NULL || value == 0)
+		return (NULL);
+
+	/*alloue un espace memoire*/
+	new_node = malloc(sizeof(binary_tree_t));
+	if (!new_node)
+		return (NULL);
+
+	new_node->n = value;
+	new_node->parent = parent;
+	new_node->left = NULL;
+	new_node->right = NULL;
+
+	/* verifie le cas ou le parent a deja un enfant droite*/
+	if (parent->right != NULL)
+	{
+		new_node->right = parent->right;
+		new_node->right->parent = new_node;
+	}
+
+	parent->right = new_node;
+	return (new_node);
+}
