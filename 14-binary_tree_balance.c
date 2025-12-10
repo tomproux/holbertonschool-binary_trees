e4184d8f850b79c2028df45441c4af3a8e5a47ee
ValPQT <valentinpasquiet@gmail.com>
2025-12-10 15:27:53 +0000

    feat : < add the new file 14-binary_tree_balance.c >

---
+++ 14-binary_tree_balance.c
@@ -0,0 +1,32 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include "binary_trees.h"
+
+int binary_tree_balance(const binary_tree_t *tree)
+{
+	size_t left_height = 0, right_height = 0;
+
+	if (tree == NULL)
+		return (0);
+
+	left_height = binary_tree_height(tree->left);
+	right_height = binary_tree_height(tree->right);
+	return (left_height - right_height);
+}
+
+
+size_t binary_tree_height(const binary_tree_t *tree)
+{
+	size_t left_size = 0;
+	size_t right_size = 0;
+
+	if (!tree)
+		return (0);
+
+	left_size += 1 + binary_tree_height(tree->left);
+	right_size += 1 + binary_tree_height(tree->right);
+	if (left_size > right_size)
+		return (left_size);
+
+	return (right_size);
+}
