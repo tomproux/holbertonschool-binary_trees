1b5da2a79750573799a9265652203751b7274a93
tomproux <tomproux53@gmail.com>
2025-12-10 07:09:34 +0000

    tache 6 a 8

--- 8-binary_tree_postorder.c
+++ 8-binary_tree_postorder.c
@@ -14,10 +14,10 @@ void binary_tree_postorder(const binary_tree_t *tree, void (*func)(int))
 	if (!tree || !func)
 		return;
 
-        /* parcours du sous-arbre gauche puis du sous-arbre droit */
+	/* parcours du sous-arbre gauche puis du sous-arbre droit */
 	binary_tree_postorder(tree->left, func);
 	binary_tree_postorder(tree->right, func);
 
 	/* on traite la valeur du noeud en premier */
-        func(tree->n);
+	func(tree->n);
 }
